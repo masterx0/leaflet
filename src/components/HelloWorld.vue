fee56d6af44a0a36c81a1ec6a093c18893efeca7
徐秀洋 <xuxiuyang@duxiaoman.com>
2021-07-14 23:13:34 +0800

    init commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="vue-leaflet">
+    <l-map style="width: 100%; height: 600px;" :zoom="zoom" :center="center">
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <l-marker :lat-lng="marker">
+        <l-popup :content="text"></l-popup>
+      </l-marker>
+    </l-map>
+
+  </div>
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker, LPopup } from 'vue2-leaflet'
+import L from 'leaflet'
+export default {
+  name: 'VueLeaflet.vue',
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup
+  },
+  data () {
+    return {
+      zoom: 13,
+      center: L.latLng(40.044184, 116.271369),
+      url: 'http://webrd01.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=7&x={x}&y={y}&z={z}',
+      attribution: '© <a href="https://lbs.amap.com/">AutoNav</a>',
+      marker: L.latLng(40.044184, 116.271369),
+      text: '这是度小满！'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
